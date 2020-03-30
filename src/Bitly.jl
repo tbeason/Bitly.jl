e5df18d91c4c75cb881ddc4e394c0fc01bb15019
Tyler Beason <tbeas12@gmail.com>
2020-03-29 17:31:25 -0700

    require 1.1 bc splitpath and docstring update

--- src/Bitly.jl
+++ src/Bitly.jl
@@ -97,7 +97,7 @@ end
 
 Exchanges your username and password for an access token. Your username and password are *not stored*!
 
-Returns a string.
+Returns an initialized [`BitlyToken`](@ref) object.
 
 Note: you can also log on to your account on Bitly and request an access token directly.
 """
